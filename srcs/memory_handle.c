f7ca8035cc01d433d27136d7e6b594ea28a6577e
Marta Pais Goncalves E Melo De La Feria <mpais-go@c1r18s1.42lisboa.com>
2024-05-02 16:35:57 +0100

    Cenas

--- srcs/memory_handle.c
+++ srcs/memory_handle.c
@@ -3,30 +3,11 @@
 /*                                                        :::      ::::::::   */
 /*   memory_handle.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jcameira <jcameira@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mpais-go <mpais-go@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/01 01:14:41 by jcameira          #+#    #+#             */
-/*   Updated: 2024/05/02 02:46:09 by jcameira         ###   ########.fr       */
+/*   Updated: 2024/05/02 16:31:24 by mpais-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <minishell.h>
 
-void	free_arr(char **array)
-{
-	int	i;
-
-	if (!array)
-		return ;
-	i = -1;
-	while (array[++i])
-		free(array[i]);
-	free(array);
-}
-
-void	exit_shell(t_minishell *msh)
-{
-	free_arr(msh->envp);
-	printf("exit\n");
-	exit(1);
-}
