e03b0a5770b3ea1095c836082498fc69931c6d40
Marta Pais Goncalves E Melo De La Feria <mpais-go@c1r17s2.42lisboa.com>
2024-05-06 19:11:41 +0100

    mini

---
+++ includes/builtins.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtins.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mpais-go <mpais-go@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/03 15:04:23 by mpais-go          #+#    #+#             */
+/*   Updated: 2024/05/06 16:56:24 by mpais-go         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUILTINS_H
+# define BUILTINS_H
+# include "minishell.h"
+
+void	mini_cd(t_minishell *msh, t_simplecmd *cmd);
+void	mini_echo(t_minishell *msh, t_simplecmd *cmd);
+void	mini_env(t_minishell *msh, t_simplecmd *cmd);
+void	mini_export(t_minishell *msh, t_simplecmd *cmd);
+void	mini_pwd(t_minishell *msh, t_simplecmd *cmd);
+void	mini_unset(t_minishell *msh, t_simplecmd *cmd);
+
+#endif
